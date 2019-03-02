ca436994f79c2e016c534a5688825d753eb54492
Michal Padzik <padzikm@gmail.com>
2019-03-02 18:18:33 +0100

    test script

---
+++ vars/shopper.groovy
@@ -0,0 +1,25 @@
+def test(imageName, testFilePath){
+    stage("build docker image"){
+        sh "docker image build --target test -t ${imageName} ."
+    }
+    
+    stage("run docker image"){
+        try{
+            sh "docker container run --name ${imageName} ${imageName}"
+        }
+        catch(ex){
+        }
+    }
+    
+    stage("copy test results"){
+        sh "docker cp ${imageName}:${testFilePath} testResults.xml"
+    }
+    
+    stage("remove docker image"){
+        sh "docker container rm ${imageName}"
+    }
+    
+    stage("verify test results"){
+        xunit thresholdMode: 2, thresholds: [failed(failureNewThreshold: '0', failureThreshold: '0', unstableNewThreshold: '0', unstableThreshold: '0')], tools: [xUnitDotNet(deleteOutputFiles: true, failIfNotNew: false, pattern: 'testResults.xml', skipNoTestFiles: false, stopProcessingIfError: true)]
+    }
+}
--- vars/shopperDocker.groovy
+++
@@ -1,1 +0,0 @@
-def funcja(msg){ println msg}
